4a6311d2d94081d5fbcfb097032c237a5a7f1cd2
muhammadYousof <my 7370044@gmail.com>
2023-10-02 12:48:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+alert("erorr! please enter a valid password");
+
+alert("welcome to js land... happy coding ");
+
+alert("welcome to js land");
+
+alert("happy coding");
+
+alert("hello... i can run JS through my web browser's console");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
